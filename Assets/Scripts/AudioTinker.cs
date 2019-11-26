1e710b9cf37ec45295fc7a767192099909ff2d47
Quinny18 <Callum_Quinn1@outlook.com>
2019-11-26 02:21:49 +0000

    Added basic functionality for contract 1.

--- Assets/Scripts/AudioTinker.cs
+++ Assets/Scripts/AudioTinker.cs
@@ -1,64 +1,72 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
-//using NaughtyAttributes;
 using UnityEngine;
 
 #if UNITY_EDITOR
 using UnityEditor;
 #endif
 
-
 public class AudioTinker : MonoBehaviour {
     private AudioSource audioSource;
     private AudioClip outAudioClip;
     
-    
+
     // Start is called before the first frame update
     void Start() {
         audioSource = GetComponent<AudioSource>();
-        outAudioClip = CreateToneAudioClip(20000);
-        PlayOutAudio(); 
+        //Creates a tone with the frequency of 20000, then plays it
+        StartCoroutine(DelaySound());
     }
     
-
-    // Public APIs
-    public void PlayOutAudio() {
-        audioSource.PlayOneShot(outAudioClip);    
-    }
-
 
     public void StopAudio() {
         audioSource.Stop();
     }
     
-    
-    // Private 
-    private AudioClip CreateToneAudioClip(int frequency) {
-        int sampleDurationSecs = 5;
-        int sampleRate = 44100;
-        int sampleLength = sampleRate * sampleDurationSecs;
+   
+    private AudioClip CreateTone(int frequency, int seconds, float sampleModifier) {
+        //How long the sound that it creates will play for
+        int sampleDurationSecs = seconds;
+
+        //Sample rate is constant
+        int sampleRate = (int) (44100 * sampleModifier);
+
         float maxValue = 1f / 4f;
-        
+        //SampleLength is the total number of samples in the audioclip
+        int sampleLength = sampleRate * (int)sampleDurationSecs;
+        //This creates an audioclip that will be edited in this function
         var audioClip = AudioClip.Create("tone", sampleLength, 1, sampleRate, false);
         
+        //Creates an array of float at the length of sampleLength
         float[] samples = new float[sampleLength];
+        //For ever sample its assigning a sound
         for (var i = 0; i < sampleLength; i++) {
+            
             float s = Mathf.Sin(2.0f * Mathf.PI * frequency * ((float) i / (float) sampleRate));
+
             float v = s * maxValue;
+
             samples[i] = v;
         }
-
+        //Assigning the audio to the audioclip
         audioClip.SetData(samples, 0);
         return audioClip;
     }
 
-    
-//#if UNITY_EDITOR
-//    [Button("Save Wav file")]
-//    private void SaveWavFile() {
-//        string path = EditorUtility.SaveFilePanel("Where do you want the wav file to go?", "", "", "wav");
-//        var audioClip = CreateToneAudioClip(1500);
-//        SaveWavUtil.Save(path, audioClip);
-//    }
-//#endif
+    //This outputs the sound
+    public void PlayOutAudio(AudioClip sound)
+    {
+        audioSource.PlayOneShot(sound);
+
+    }
+
+    IEnumerator DelaySound()
+    {
+        PlayOutAudio(CreateTone(1975 + Random.Range(-500, 500), 1, 0.25f));
+        yield return new WaitForSeconds(0.15f);
+        PlayOutAudio(CreateTone(2637 + Random.Range(-500, 500), 1, 0.25f));
+        yield return new WaitForSeconds(0.25f);
+        StopAudio();
+    }
+
 }
